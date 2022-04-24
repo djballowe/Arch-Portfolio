cd362521a6f5a961a38e23fb6f4189ae8c562876
djballowe <djballowe@gmail.com>
2022-04-23 18:49:58 -0700

    last commit 4/23

--- Resources/Slideshow.js
+++ Resources/Slideshow.js
@@ -1,15 +1,23 @@
-var slideIndex = 0;
-showSlides();
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++)
-    {
-        slides[i].style.display = "none";
-    } 
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(int) {
+    showSlides(slideIndex(int));
+}
+
+function showSlides(int) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (int > slides.length) {
+        slideIndex = 1;
     }
-    slides[slideIndex - 1].style.display = "block";
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
 }
