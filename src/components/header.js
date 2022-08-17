4b33efeae0ddf183ee381d576ffb7148dd0b20e7
djballowe <djballowe@gmail.com>
2022-08-17 15:24:50 -0700

    header layouts

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,19 @@
 import React from "react";
+import logo from "../Images/logo vector.svg";
 
 const Header = () => {
   return (
     <div>
-      <div className="logo"></div>
+      <div className="logo">
+        <img src={logo} alt="" />
+        <p>DAVID BALLOWE</p>
+      </div>
       <ul className="nav">
         <li>Home</li>
         <li>Portfolio</li>
         <li>About</li>
         <li>Contact</li>
+        <li>Blog</li>
       </ul>
     </div>
   );
