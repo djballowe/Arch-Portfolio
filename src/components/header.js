a919e3ad21de5657e5d753410371096b4e6ac1a6
djballowe <djballowe@gmail.com>
2022-08-17 14:58:08 -0700

    creating initial layout for header

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="logo"></div>
+      <ul className="nav">
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
