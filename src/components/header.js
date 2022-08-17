8b3c9d7d223912bde3fc9ef6434c106769b8c10c
djballowe <djballowe@gmail.com>
2022-08-17 15:51:59 -0700

    adding mobile styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import logo from "../Images/logo vector.svg";
+import Dropdown from "./Dropdown";
+import Menu from "@mdi/react";
+import { mdiMenu } from "@mdi/js";
+import MobileMenu from "./MobileMenu";
 
 const Header = () => {
   return (
     <div>
+      <MobileMenu />
       <div className="logo">
         <img src={logo} alt="" />
+        <Menu path={mdiMenu} size={1.5} />
         <p>DAVID BALLOWE</p>
       </div>
       <ul className="nav">
         <li>Home</li>
         <li>Portfolio</li>
+        <Dropdown />
         <li>About</li>
         <li>Contact</li>
         <li>Blog</li>
