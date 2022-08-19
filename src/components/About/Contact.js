51f6143a7507c7933f0dc6f04039a3df2e423a67
djballowe <djballowe@gmail.com>
2022-08-19 11:24:34 -0700

    adding mobile styles for contact form

--- src/components/About/Contact.js
+++ src/components/About/Contact.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contacat</div>;
+  return (
+    <div className="contact-container">
+      <div className="contact-text">
+        <p>Contact</p>
+        <p>
+          For all pricing and booking inquires, please fill out the form below.
+          Include as much relevant information as possible. ie. location of
+          project, amount of images required, the intended use of the images,
+          and the relative timeline the project needs to take place. I usually
+          get back within 48 hours. I look forward to hearing from you.
+        </p>
+      </div>
+      <div>
+        <label htmlFor="">Name</label>
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <label htmlFor="">Email</label>
+        <input type="text" />
+        <input type="text" />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
