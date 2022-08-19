77c27d26f132e9edaf5954412b886a2285f6202f
djballowe <djballowe@gmail.com>
2022-08-19 09:38:21 -0700

    routs added for all main pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import DsMenu from "./components/Header/DsMenu";
 import Header from "./components/Header/header";
 import Home from "./components/Home/Home";
 import Interiors from "./components/Interiors/Interiors";
+import Exteriors from "./components/Exteriors/Exteriors";
+import About from "./components/About/About";
+import Contact from "./components/About/Contact";
 
 function App() {
   return (
@@ -14,6 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="interiors" element={<Interiors />} />
+          <Route path="exteriors" element={<Exteriors />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+
         </Routes>
       </div>
     </div>
