97ea3c45ef68cfbd6090d1ea249ed4711114a922
djballowe <djballowe@gmail.com>
2022-08-19 11:06:14 -0700

    responsive on mobile and desktop

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
       </div>
       <div className="profile-text">
         <p>"If a building becomes architecture, then it is art"</p>
-        <p>Arne Jacobsen</p>
+        <p>- Arne Jacobsen</p>
         <p>Photographer, 3d artist, web-design</p>
         <p>Email: db@davidballowe.com</p>
         <p>Instagram: @djballowe</p>
