1a85ca0be24989ac54217d5e8864743c2a7ad97a
djballowe <djballowe@gmail.com>
2022-08-23 16:40:22 -0700

    add images to slideshow json

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+const Images = require("./SlideImages.json").images;
 
 export default function Home() {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleClick = (e) => {
+    const button = e.target.id;
+    button === "+"
+      ? setCurrentImage(currentImage + 1)
+      : setCurrentImage(currentImage - 1);
+  };
+
   return (
     <div>
-      <div className="slide-container">
-        <img src={require("../../Images/SlideShow/slide3.JPG")} alt="" />
+      <div className="slide-container" onClick={handleClick} id="+">
+        <img
+          onClick={handleClick}
+          id="+"
+          src={require(`../../Images/SlideShow/${Images[currentImage].src}`)}
+        />
       </div>
       <div className="slide-controls">
-        <p>Prev</p>
+        <p onClick={handleClick} id="-">
+          Prev
+        </p>
         <p>/</p>
-        <p>Next</p>
+        <p onClick={handleClick} id="+">
+          Next
+        </p>
       </div>
     </div>
   );
