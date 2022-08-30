5e7e9c1c18a35ad398c809236d6ee63f6fd722f9
djballowe <djballowe@gmail.com>
2022-08-30 11:18:36 -0700

    linked added for email and instagram

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -11,33 +11,29 @@ const Header = (props) => {
     props.open();
   };
 
-  if (window.location.pathname === "/interiors/interiors-slide") {
-    return null;
-  } else {
-    return (
-      <div className="header">
-        <div className="logo">
-          <img
-            src={logo}
-            alt=""
-            onClick={() => {
-              navigate("/");
-            }}
-          />
-          <div className="mobile-menu-container">
-            <Menu path={mdiMenu} size={1.5} onClick={handleClick} />
-          </div>
-          <p
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            DAVID BALLOWE
-          </p>
+  return (
+    <div className="header">
+      <div className="logo">
+        <img
+          src={logo}
+          alt=""
+          onClick={() => {
+            navigate("/");
+          }}
+        />
+        <div className="mobile-menu-container">
+          <Menu path={mdiMenu} size={1.5} onClick={handleClick} />
         </div>
+        <p
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          DAVID BALLOWE
+        </p>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Header;
